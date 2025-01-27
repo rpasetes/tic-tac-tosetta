7a418bc8b858533d3f7a7589ea2afdd1abbf32a8
rpasetes <russap01@gmail.co>
2025-01-27 15:59:14 -0500

    react with michael

---
+++ react-vite/src/App.jsx
@@ -0,0 +1,117 @@
+import { useState } from "react"
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0) // how you're placed in time
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove]; 
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1)
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  };
+
+  const moves = history.map((squares, move) => {
+    let description = (move > 0)
+     ? `Go to move #${move}`
+     : `Go to game start`
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) { return } // guard pattern noice
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? "X" : "O"; 
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  // ternary operator multiline so clean 
+  let status = (winner) 
+    ? `Winner: ${winner}` // and string formatting wutttt
+    : `Current Player: ${xIsNext ? "X" : "O"}`
+
+  return (
+    <>
+      <h1 className="status">{status}</h1>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  )
+}
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button 
+      className="square"
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+// REFACTORING NOTES
+// use List.at() for negative indexing,
+
+// FOR NEXT TIME:
+// push incremental changes
+// to track interesting syntax shifts over time
+// rather than breezing through implementation in bursts
+// and only committing final versions of code
+// seeing how code evolves over time seems cool...
